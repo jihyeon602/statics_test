a1eec100dc562d757c952454f524c4f17acdc5e9
jihyeon602 <000602jh@naver.com>
2025-03-24 14:07:39 +0900

    머신러닝-지도학습(회귀)

---
+++ practice/Supervised_Learning-Regression/q2.py
@@ -0,0 +1,87 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# 데이터를 분리하는 모듈을 불러옵니다.
+from sklearn.model_selection import train_test_split
+
+# 사이킷런에 구현되어 있는 회귀 모델을 불러옵니다.
+from sklearn.linear_model import LinearRegression
+
+"""
+1. 데이터를 생성하고, 
+   생성한 데이터를 
+   학습용 데이터와 테스트용 데이터로 분리하여 
+   반환하는 함수를 구현합니다.
+"""
+def load_data():
+    
+    np.random.seed(0)
+    
+    X = 5*np.random.rand(100,1)
+    y = 3*X + 5*np.random.rand(100,1)
+    
+    train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.3, random_state=0)
+    
+    return train_X, test_X, train_y, test_y
+
+"""
+2. 단순 선형회귀 모델을 불러오고, 
+   불러온 모델을 학습용 데이터에 
+   맞추어 학습시킨 후
+   테스트 데이터에 대한 
+   예측값을 반환하는 함수를 구현합니다.
+"""
+def regression_model(train_X, train_y):
+    
+    simplelinear = LinearRegression()   #모델 생성
+    
+    simplelinear.fit(train_X, train_y)  #모델학습
+    
+    return simplelinear
+    
+# 그래프를 시각화하는 함수입니다.
+def plotting_graph(train_X, test_X, train_y, test_y, predicted):
+    fig, ax = plt.subplots(1,2, figsize=(16, 7))
+    
+    ax[0].scatter(train_X,train_y)
+    ax[1].scatter(test_X,test_y)
+    ax[1].plot(test_X, predicted, color='b')
+    
+    ax[0].set_xlabel('train_X')
+    ax[0].set_ylabel('train_y')
+    ax[1].set_xlabel('test_X')
+    ax[1].set_ylabel('test_y')
+    
+    fig.savefig("practice\Supervised_Learning-Regression\q2_result.png")
+    
+"""
+3. 모델 학습 및 예측 결과 확인을 위한 
+   main() 함수를 완성합니다.
+"""
+def main():
+    
+    train_X, test_X, train_y, test_y = load_data()
+    
+    simplelinear = regression_model(train_X, train_y)
+    
+    predicted = simplelinear.predict(test_X)          #테스트데이터에 대한 예측
+    
+    model_score = simplelinear.score(test_X, test_y)  #모델 평가점수
+    
+    beta_0 = simplelinear.intercept_  #학습이 완료된 모델의 𝛽0 반환
+    beta_1 = simplelinear.coef_       ##학습이 완료된 모델의 𝛽1 반환
+    
+    print("> beta_0 : ",beta_0)
+    print("> beta_1 : ",beta_1)
+    
+    print("> 모델 평가 점수 :", model_score)
+    
+    # 시각화 함수 호출하기
+    plotting_graph(train_X, test_X, train_y, test_y, predicted)
+    
+    return predicted, beta_0, beta_1, model_score
+    
+    
+if __name__=="__main__":
+    main()
